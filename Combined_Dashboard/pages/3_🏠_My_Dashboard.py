678aa108a1cb644d5d873ecf307e52509ec185cd
Sushil <sushil.ray11@gmail.com>
2026-08-31 14:30:42 +0530

    change sync

--- Combined_Dashboard/pages/3_🏠_My_Dashboard.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-_DIR = "/Users/I325211/Local_Project/My_dashboard"
-_app_path = os.path.join(_DIR, "app.py")
-if _DIR not in sys.path:
-    sys.path.insert(0, _DIR)
-
-exec(open(_app_path).read(), {"__file__": _app_path})
